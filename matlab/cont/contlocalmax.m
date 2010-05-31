4c9ab62ac7fc0fb17d528e08936944c0d9520814
Tom Davidson <tjd@stanford.edu>
2010-05-30 20:56:32 -0700

    contlocalmax: only generate 2nd arg when requested, handle channels with no peaks

--- matlab/cont/contlocalmax.m
+++ matlab/cont/contlocalmax.m
@@ -6,8 +6,14 @@ function [lindex time_ind_val] = contlocalmax(cont)
   
   % prefix the time of each event to the list, so each event is
   % represented on a row as: time, index, value
-  for k = 1:length(ind_val),
-    time_ind_val{k} = zeros(size(ind_val{k},1),size(ind_val{k},2)+1);
-    time_ind_val{k}(:,1) = ((ind_val{k}(:,1)-1)./cont.samplerate) + cont.tstart;
-    time_ind_val{k}(:,[2 3]) = ind_val{k};
+  if nargout > 1
+    for k = 1:length(ind_val),
+      if ~isempty(ind_val{k})
+        time_ind_val{k} = zeros(size(ind_val{k},1),size(ind_val{k},2)+1);
+        time_ind_val{k}(:,1) = ((ind_val{k}(:,1)-1)./cont.samplerate) + cont.tstart;
+        time_ind_val{k}(:,[2 3]) = ind_val{k};
+      else
+        time_ind_val{k} = [];
+      end
+    end
   end
