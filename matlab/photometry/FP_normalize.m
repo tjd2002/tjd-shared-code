fa54d93b7edaab4422f81974196ee4f3716912c7
Tom Davidson <tjd@stanford.edu>
2015-03-24 16:08:15 -0700

    Move FP code to shared-code from tjd-matlab

---
+++ matlab/photometry/FP_normalize.m
@@ -0,0 +1,171 @@
+function [c_out c_Regress bls YFit] = FP_normalize(c_Mag, varargin)
+% FP_NORMALIZE: normalize a photometry signal using isosbestic channel
+% 
+% [c_dFF c_Regress bls YFit filt] = FP_normalize(c_Mag, varargin)
+%
+% REQUIRED INPUTS:
+%  c_Mag: 2-channel cont struct, with signal in ch1, isosbestic control in ch2
+%
+%  Param/Value pairs.
+%  'control_LP_F': transition band for LPF to apply to the control signal
+%                  before fitting (Hz; no default, try: [0.1 0.2]);
+%
+% OPTIONAL INPUTS:
+%     'norm_type': dF/F denominator: control channel fit (default), constant 
+%                  value, or no normalization (see code for discussion): 
+%                  ({'fit'}, 'const', 'none')
+%    'rig_baseline_V': power in each channel before plugging in the animal
+%                  (will be subtracted before normalizing, default [0 0])
+% 'dFF_zero_prctile': re-zero dFF signal to this percentile of the final
+%                  signal. Empty means do not re-zero (default: 1)
+% 'original_timewin': whether to return all data, including invalid regions
+%                  due to filter edge effects (default: false)
+%
+% NB: the beginning and end of the signal may be invalid due to filter edge
+% effects, and are excluded by default. Lower frequency filters will result
+% in longer excluded regions.
+%
+% Example:
+%
+%   c_Mag = contdemodulate(c_FP_Raw,...
+%                          'nsignals', 2,'bandwidth_F', [10 15])
+%
+%   c_dFF = FP_normalize(c_Mag,...
+%                        'control_LP_F', [0.1 0.2],...
+%                        'norm_type', 'fit');
+%
+%   c_dFF = contwin(c_dFF,[],'samps_good');
+%
+% Tom Davidson, Stanford University, Feb 2015. <tjd@alum.mit.edu>
+
+
+a = struct(...
+    'control_LP_F', [],...
+    'norm_type', 'fit',...
+    'rig_baseline_V', [0 0],...
+    'dFF_zero_prctile', 1,...
+    'original_timewin', false);
+
+a = parseArgsLite(varargin, a);
+
+if isempty(a.control_LP_F)
+    error('Must provide transition band for lowpass filter ''control_LP_F''. Try: [0.1 0.2]');
+end
+
+switch c_Mag.units
+    case {'V'}, 
+        c_Mag.units = 'V';
+    case 'mV',
+        c_Mag.data = c_Mag.data./1000;
+        c_Mag.units = 'V';
+    otherwise
+        error('Unrecognized ''units'' in c_Mag');
+end
+%% Normalize using 'isosbestic' channel
+% Debleaching below works well. Does not make assumptions 
+% about dynamics (other than speed, set by LPF), so we can follow unusual 
+% bleaching curves.
+
+% Signal to detrend is in chan #1, isosbestic control is in chan 2;
+ch_sig = 1;
+ch_iso = 2;
+
+% Remove baseline from each signal
+c_Mag.data = bsxfun(@minus, c_Mag.data, a.rig_baseline_V([ch_sig ch_iso]));
+c_Mag = contdatarange(c_Mag);
+
+% downsample, apply 'very-low-pass' filter, then upsample
+fopt_vlp = mkfiltopt('filttype', 'lowpass', 'F', a.control_LP_F, 'name', 'VLP','atten_db', 40);
+c_Mag_F_ds = contfilt(c_Mag, 'filtopt', fopt_vlp, 'autoresample', true);
+
+% upsample smoothed control signal to match original
+c_Mag_F = continterp(c_Mag_F_ds,...
+      'timewin', [c_Mag.tstart c_Mag.tend], ...
+      'samplerate', c_Mag.samplerate,...
+      'extrapval', 0);
+  
+% Select channels to regress 
+% (Signal to detrend is in chan #1, isosbestic control is in chan 2)
+c_Regress = contcombine(...
+    contchans(c_Mag,'chans', ch_sig),... 
+    contchans(c_Mag_F, 'chans', ch_iso));
+c_Regress.chanlabels = {'Y_signal', 'XX_isosbestic_control'};
+
+% Filtering results in edge effects--we don't want to include this data in
+% our fit. (contfilt/continterp/contcombine keep track of this
+% automatically)
+c_Regress = contwin(c_Regress, [], 'samps_good');
+
+% regress FP signal against 405 control to learn coeffs:
+% (if numerical problems, try subsampling data)
+XX = c_Regress.data(:,ch_iso); % smoothed control channel
+Y = c_Regress.data(:,ch_sig); % original signal channel to fit
+bls = polyfit(XX,Y,1);
+
+% Model all data (including invalid points not used in the regression)
+XX_all = c_Mag_F.data(:,ch_iso); % smoothed control
+Y_all = c_Mag.data(:,ch_sig); % original signal
+
+Y_fit_all = bls(1) .* XX_all + bls(2);
+
+% Subtract Y_fit to get the residual 'transients' (in detector units,
+% i.e. Volts)
+Y_dF_all = Y_all - Y_fit_all;
+
+% 2 options for normalization of deltaF:
+%
+switch a.norm_type
+
+    case 'const'
+        % OPTION 1:
+        % constant scaling assumes that GCaMP residual doesn't bleach, so
+        % transients don't need to be scaled
+        
+        % scale deltaF by mean of (valid points in) signal.
+        Y_dFF_all = Y_dF_all ./ mean(Y);
+        suffix = 'dFF';
+
+    case 'fit'
+        % OPTION 2:
+        % dividing through by Y_fit assumes that GCaMP bleaching scales with
+        % overall tissue bleaching.
+        Y_dFF_all = Y_dF_all./Y_fit_all; % deltaF/F for each timepoint
+        suffix = 'dFF';
+
+    case 'none'
+        % OPTION 3:
+        % Don't scale; return dF, in detector units        
+        Y_dFF_all = Y_dF_all;
+        suffix = 'dF';
+
+        
+    otherwise
+        error('Unrecognized ''norm_type'' argument');
+end
+
+% Optionally shift baselin (to nth percentile of valid data) to make 
+% comparable to usual deltaF/F measures
+if ~isempty(a.dFF_zero_prctile);
+    Y_dFF_valid = Y_dFF_all([c_Mag_F.nbad_start+1:end-c_Mag_F.nbad_end]);
+    Y_dFF_all = Y_dFF_all - prctile(Y_dFF_valid,a.dFF_zero_prctile);
+end
+
+% save out cont struct:
+c_out = mkcdat(...
+    'name', [c_Mag.name '_' suffix],...
+    'chanlabels', {[c_Mag.chanlabels{ch_sig} '_' suffix]},...
+    'samplerate', c_Mag.samplerate,...
+    'tstart', c_Mag.tstart,...
+    'tend', c_Mag.tend,...
+    ... % c_Mag_F signal used to make dFF, has largest invalid regions
+    'nbad_start', c_Mag_F.nbad_start,...
+    'nbad_end', c_Mag_F.nbad_end,...
+    'max_tserr', c_Mag.max_tserr,...
+    'data', Y_dFF_all);
+c_out = contdatarange(c_out);
+
+if ~a.original_timewin,
+    c_out = contwin(c_out, [], 'samps_good');
+end
+
+contcheck(c_out);
---
+++ matlab/photometry/contdemodulate.m
@@ -0,0 +1,259 @@
+function [c_Mag, Ref_F, PSDs, cache] = contdemodulate(c_Raw, varargin)
+% CONTDEMODULATE: do amplitude demodulation
+%
+% [c_Mag, Ref_F, PSDs, cache],...
+%      = contdemodulate(c_Raw, <param/value pairs>);
+%
+% INPUTS:
+% c_Raw: cont struct with the following named channels:
+%           'Det1': Raw detector signal to be demodulated 
+%          'Ref1X': carrier sinusoid for signal 1
+%          'Ref1Y': 90-degrees out of phase carrier sinusoid for signal 1
+%          'Ref2X': carrier sinusoid for signal 2
+%          'Ref2Y': 90-degrees out of phase carrier sinusoid for signal 2
+%            ...etc
+%
+% REQUIRED PARAMETERS:
+%       'nsignals': How many signals are we demodulating? (no default)
+%    'bandwidth_F': Low-pass transition band, in Hz, for frequencies of 
+%                   interest in original signal. Mx2, one row per signal. 
+%                   If only one row, use same filter for each signal.
+%                   (no default, try [10 15])
+%
+% OPTIONAL PARAMETERS:
+%'detector_chanlabel': Name of detector channel to decode (default: 'Det1')
+%        'atten_db': Requested stopband attenuation of filters, in dB 
+%                    (default = -50) (NB multiple filters in series)
+%         'ripp_db': Requested passband ripple of filters, in dB 
+%                    (default = 0.1) (NB multiple filters in series)
+%      'LPF_repeat': How many times to apply the low-pass filter after the
+%                    product detectors (default = 2)
+%'recover_carriers_from_signal': Recover carrier frequencies from input
+%                    signal, ignore provided references. (default: false)
+%           'cache': object cache containing previously-designed filters,
+%                    etc.
+%
+% OUTPUTS:
+%         c_Mag: cont struct, with one channel per demodulated signal
+%         Ref_F: array of recovered reference frequencies for each signal
+%
+% ADDITIONAL DEBUGGING OUTPUTS:
+%          PSDs: struct containg spectra of various intermediate signals
+%         cache: contains named filters used at various stages
+%
+% EXAMPLES:
+%  [c_Mag RefF] = contdemodulate(c_FP_Raw, ...
+%                                'nsignals', 2, ...
+%                                'bandwidth_F', [10 15]);
+%
+% By: Tom Davidson, Stanford University, 2015 (tjd@alum.mit.edu)
+
+% TODO:
+% -pass out estimated resultant Freqz for all processing.
+% -Implement with iFFT instead of bandpass/product detectorr/LPF
+% -Generate references from given frequencies (avoid recording RefX),
+%  needed for recover_carriers_from_signal', too.
+% -Handle DC as special case? Freq 0? Same LPF (code commented out below)
+%
+% LATER
+% -zero-padding/continterp
+% -Demodulate multiple detectors (prefilter/choose sampling rates based 
+%  on all detectors to avoid aliasing)
+%
+% DONE
+% -Optionally recover frequencies from raw detector signal?
+% -Pass out power spectra of detector, demodulated signals
+% -LPF loop
+% -Pass out power spectra of detector, demodulated signals
+
+
+  % data integrity check
+  contcheck(c_Raw);
+  
+  a = struct (...
+      'detector_chanlabel', 'Det1',...
+      'nsignals', [],...
+      'bandwidth_F', [],...
+      'recover_carriers_from_signal', false,...
+      'atten_db', -50,...
+      'ripp_db', 0.1,...
+      'LPF_repeat', 2,...
+      'cache', mkcache());
+      
+  a = parseArgsLite(varargin, a);
+
+%% Recover modulation frequencies 
+
+% Calculate the input (detector) PSD even if we don't need it below.
+c_temp = contchans(c_Raw, 'chanlabels', a.detector_chanlabel);
+[PSDs.Pxx_Det, PSDs.F_Det] = periodogram(c_temp.data,blackman(size(c_temp.data,1)),[],c_temp.samplerate);
+[PSDs.Pxx_Det_d, PSDs.F_Det_d] = periodogram(detrend(c_temp.data, 'constant'),blackman(size(c_temp.data,1)),[],c_temp.samplerate);
+
+if a.recover_carriers_from_signal, 
+    
+    % Recover nsignals frequencies with most power from the detector signal
+    [~,Ppks_val] = localmax(PSDs.Pxx_Det);
+    Ppks_rank = sortrows(Ppks_val{1},-2);
+    Ref_F = PSDs.F_Det(Ppks_rank(1:a.nsignals,1));
+    
+    % Discard unneeded channels from c_Raw:
+    c_Raw = contchans(c_Raw, 'chanlabels', a.detector_chanlabel);
+
+    % Have yet to write code to synthesize reference frequencies below
+    disp('Demodulation without reference signals not supported yet, returning recovered ref frequencies only.');
+    c_Mag = [];
+    return
+
+else % Recover frequencies from the individual modulation channels
+    
+    % Ensure we have all the references we'll need
+    for j = 1:a.nsignals;
+        RefXstr{j} = ['Ref' num2str(j) 'X'];
+        RefYstr{j} = ['Ref' num2str(j) 'Y'];
+        if sum(strcmp(RefXstr{j}, c_Raw.chanlabels)) ~=1,
+            error('Must provide reference channels named: ''%s''', RefXstr{j});
+        end
+        if sum(strcmp(RefYstr{j}, c_Raw.chanlabels)) ~=1,
+            error('Must provide reference channel named: ''%s''', RefYstr{j});
+        end
+    end
+    
+    % Find peak of FFT in each RefX signal    
+    for j = 1:a.nsignals;    
+        chanidx = chansfromlabels(c_Raw, RefXstr{j});
+        [PSDs.Pxx_Ref(:,j), PSDs.F_Ref(:,j)] = ...
+            periodogram(c_Raw.data(:,chanidx),blackman(size(c_temp.data,1)),[],c_Raw.samplerate);
+        [~,maxidx] = max(PSDs.Pxx_Ref(:,j));
+        Ref_F(j) = PSDs.F_Ref(maxidx); %#ok
+    end
+    
+    % Discard unneeded channels from c_Raw:
+    c_Raw = contchans(c_Raw, 'chanlabels', {a.detector_chanlabel RefXstr{:} RefYstr{:}});
+    
+end
+
+
+%% Analysis parameters setup
+
+% Use same LPF for all signals if only one provided
+if size(a.bandwidth_F,1)==1,
+    a.bandwidth_F = repmat(a.bandwidth_F,a.nsignals,1);
+end
+if size(a.bandwidth_F,1)~=a.nsignals,
+    error('Must provide either one ''bandwidth_F'', or one per demodulation frequency');
+end
+
+for j = 1:a.nsignals;
+    % Low-pass filter design parameters (transition band, in Hz)
+    fopt_LPF(j) = mkfiltopt(...
+        'name', sprintf('LPF%d', j),...
+        'filttype', 'lowpass', ...
+        'F', a.bandwidth_F(j,:),...
+        'atten_db', a.atten_db,...
+        'ripp_db', a.ripp_db);
+
+    % Design bandpass filters for each signal to select modulated
+    % signal+sidebands (should these be wider?)
+    fopt_prefilter(j) = mkfiltopt(... 
+        'name', sprintf('BPF%d', j),...
+        'filttype', 'bandpass', ...
+        'F', Ref_F(j) + [-fliplr(a.bandwidth_F(j,:)) a.bandwidth_F(j,:)],...
+        'atten_db', a.atten_db,...
+        'ripp_db', a.ripp_db); %#ok
+end
+
+% Downsample raw detector channel as needed (to ~3x the max ref frequency +
+% upper sideband, so still oversampled)
+
+c_Raw_ds = contresamp(...
+    c_Raw, ...
+    'resample', double(3*(max(Ref_F)+max(a.bandwidth_F(:,2))) ./ c_Raw.samplerate));
+
+% % Skip downsampling for now, until we understand performance of demodulator
+% % better
+% c_Raw_ds = c_Raw;
+
+c_Det = contchans(c_Raw_ds, 'chanlabels', a.detector_chanlabel);
+
+% %% If this is an expt with no modulation, just filter the raw detector signal
+
+% This should be behind a switch for expts with no modulation?
+% Filter the unmodulated detector signal, just for comparison
+for j = 1:a.nsignals,
+    c_Det_f(j) = c_Det; % initialize
+    for k = 1:a.LPF_repeat,
+        c_Det_f(j) = contfilt(c_Det_f(j), 'filtopt', fopt_LPF(j), 'autoresample', true);
+    end
+end
+
+% Hack to save this for output
+PSDs.c_Det_f = c_Det_f;
+
+%% Recover magnitude by demodulating.
+
+% Loop over all requested demodulation frequencies
+for j = 1:a.nsignals,
+    
+    % Prefilter with a bandpass before the product detectors to get
+    % rid of large signal from other modulated signal.
+    % (Don't resample so we can multiply easily with the references)
+    [c_Det_pf(j), prefilt(j)] = contfilt(c_Det, ...
+        'filtopt', fopt_prefilter(j), ...
+        'autoresample', false,...
+        'cache', a.cache); %#ok
+    a.cache = mkcache('cache', a.cache, 'add_obj', prefilt(j));
+    
+    % In-phase product detector. pseudocode: X = Det.*RefX (-> LPF)xN;
+    % (see SUBFUNCTIONS, below);
+    [c_X(j), a.cache] = subf_demodulate(...
+        c_Det_pf(j),... % prefiltered detector signal
+        contchans(c_Raw_ds,'chanlabels',['Ref' num2str(j) 'X']),...  % RefX
+        fopt_LPF(j),... % Lowpass filter specs
+        a.LPF_repeat,...
+        a.cache); % object cache
+
+    % In-phase product detector. pseudocode: Y = Det.*RefY (-> LPF)xN;
+    [c_Y(j), a.cache] = subf_demodulate(...
+        c_Det_pf(j),... % prefiltered detector signal
+        contchans(c_Raw_ds,'chanlabels',['Ref' num2str(j) 'Y']),... % RefY
+        fopt_LPF(j),... % Lowpass filter specs
+        a.LPF_repeat,...
+        a.cache); % object cache
+    
+    % Add the estimates of X & Y in quadrature to recover magnitude R
+    c_Mag(j) = contoperator(c_X(j), c_Y(j),...
+        'op', @hypot); %#ok
+    
+    fprintf('Done for channel %d, Ref Freq %0.2f Hz\n\n', j, Ref_F(j));
+end
+
+c_Mag = contcombine(c_Mag(1), c_Mag(2:end), 'name', [c_Raw.name '_Demod']);
+
+% debug: upsample to original input frequency and take PSD:
+c_temp = continterp(c_Mag, 'samplerate', c_Raw.samplerate);
+for j = 1:a.nsignals
+    [PSDs.Pxx_Mag(:,j), PSDs.F_Mag(:,j)] = periodogram(c_temp.data(:,j),blackman(size(c_temp.data,1)),[],c_temp.samplerate);
+end
+
+cache = a.cache; % for output
+
+% data integrity check
+contcheck(c_Mag);
+
+%% SUBFUNCTIONS
+function [c_Demod, cache] = subf_demodulate(c_Det_pf, c_Ref, fopt_LPF, LPF_repeat, cache)
+
+% Take product of signal and ref
+c_Demod = contoperator(c_Det_pf, c_Ref, ...
+    'op', @times, 'infix', '*');
+
+% Apply the post-demodulation LPF as many times as requested
+for k = 1:LPF_repeat
+    [c_Demod, lpfilt] = contfilt(c_Demod, ...
+        'filtopt', fopt_LPF, ...
+        'autoresample', true, ...
+        'cache', cache);
+    cache = mkcache('cache', cache, 'add_obj', lpfilt);
+end
+
+  
