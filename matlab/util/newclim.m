a0144f6f72ae7779c541a6b99d9cf8d3b5acaa9f
Tom Davidson <tjd@alum.mit.edu>
2019-05-28 10:29:29 -0700

    Move newclim.m from tjd-matlab

---
+++ matlab/util/newclim.m
@@ -0,0 +1,16 @@
+function CLim = newclim(BeginSlot,EndSlot,CDmin,CDmax,CmLength)
+% NEWCLIM - get clim to use subset of colormap
+% code from Matlab docs
+
+%                Convert slot number and range
+%                to percent of colormap
+PBeginSlot    = (BeginSlot - 1) / (CmLength - 1);
+PEndSlot      = (EndSlot - 1) / (CmLength - 1);
+PCmRange      = PEndSlot - PBeginSlot;
+%                Determine range and min and max 
+%                of new CLim values
+DataRange     = CDmax - CDmin;
+ClimRange     = DataRange / PCmRange;
+NewCmin       = CDmin - (PBeginSlot * ClimRange);
+NewCmax       = CDmax + (1 - PEndSlot) * ClimRange;
+CLim          = [NewCmin,NewCmax];
